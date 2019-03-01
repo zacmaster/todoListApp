3051a30b7db6a4133caa81a3124045e604742a75
Zacarías Samaniego <zacariassamaniego@gmail.com>
2019-02-28 23:52:38 -0300

    first commit

---
+++ src/App/index.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            texto: '',
+            tareas: [],
+            idSeleccionado: null
+        };
+        this.agregarTarea = this.agregarTarea.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.eliminarTarea = this.eliminarTarea.bind(this)
+        this.editarTarea = this.editarTarea.bind(this)
+    }
+
+
+    handleSubmit(e){
+        e.preventDefault()
+        this.agregarTarea(this.state.idSeleccionado)
+    }
+
+
+
+    agregarTarea(id){
+        let tareas = this.state.tareas
+        let texto = this.state.texto
+
+        id ? tareas = tareas.filter(t => t.props.id !== id) : id = Date.now()  
+        
+        tareas.unshift(
+            <div key={id} id={id}>
+                <span>{texto}</span>
+                <button onClick={() => this.editarTarea(id,texto)}>Editar</button>
+                <button onClick={() =>  this.eliminarTarea(id)}>Eliminar</button>
+            </div>
+        )
+        this.setState({tareas: tareas, texto: '', idSeleccionado: null})
+    }
+
+
+    editarTarea(id, texto){
+        this.setState({idSeleccionado: id, texto: texto})
+    }
+
+    eliminarTarea(id){
+        let tareas = this.state.tareas
+        tareas = tareas.filter(t => {
+            return parseInt(t.key) !== id
+        })
+        this.setState({tareas: tareas})
+    }   
+
+
+    handleChange({target}){
+        this.setState({texto: target.value})
+    }
+
+
+
+    render() {
+        return(
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.texto} onChange={this.handleChange}/>
+                    <input type="submit" value="Agregar"/>
+                </form>
+                <div>
+                    {this.state.tareas}
+                </div>
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+
+};
+
+export default App;
