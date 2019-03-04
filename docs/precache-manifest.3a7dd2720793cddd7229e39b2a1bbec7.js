7beea47d5e57dd4b47fdd7dc4d9a7850fa351cce
Zacarías Samaniego <zacariassamaniego@gmail.com>
2019-03-03 21:59:38 -0300

    Se agrega el deploy

---
+++ docs/precache-manifest.3a7dd2720793cddd7229e39b2a1bbec7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6a0aa35129fc7a6283db",
+    "url": "/static/js/main.6a0aa351.chunk.js"
+  },
+  {
+    "revision": "ebaa03f768964ddd5c46",
+    "url": "/static/js/1.ebaa03f7.chunk.js"
+  },
+  {
+    "revision": "6a0aa35129fc7a6283db",
+    "url": "/static/css/main.4e93c3b2.chunk.css"
+  },
+  {
+    "revision": "3cadb2955ac43684fdef893cadfd6596",
+    "url": "/index.html"
+  }
+];
